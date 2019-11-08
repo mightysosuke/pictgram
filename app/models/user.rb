381e08cee35fb9079d1790f32e4be64f08a60b42
sosuke <sosuke.sin@gmail.com>
2019-11-08 09:12:02 +0000

    Rails04の課題コミット_2回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :name, presence: true, length: {maximum: 15}
-    validates :email, presence: true, format: {with: /^[a-zA-Z0-9.!\/$%&*+\/=?^_{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/}
+    validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
     validates :password, length: {in: 8..32}, format: {with: /(?=.*[0-9])(?=.*[a-zA-Z])/}
     validates :password_confirmation, length: {in: 8..32}, format: {with: /(?=.*[0-9])(?=.*[a-zA-Z])/}
     
